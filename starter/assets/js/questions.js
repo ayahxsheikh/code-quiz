e93d16aac69ea27bd58c6feb3264b0e94e5527c1
cc-aeya <aaminah.x@icloud.com>
2022-12-05 18:39:32 +0000

    added 4 questions and answers

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -6,3 +6,28 @@
 // e.g. <button class = choice value = strings> 1. Strings </button>
 
 // use class=hide on elements you need to hide
+
+
+var questions = [
+    {
+        title: 'What does the acronym JS mean?',
+        choices: ['javaStrings', 'jovaScript', 'JS', 'javaScript'],
+        answer: 'JavaScript'
+    },
+    { 
+        title: 'Whch one of these is not a JavaScript type?',
+        choices: ['element', 'strings', 'boolean', 'numbers'],
+        answer: 'element'
+
+    },
+    {
+        title: 'What is considered a loop in JS?',
+        choices: ['for loop', 'for of loop', 'for in loop', 'all of the above'],
+        answer: 'all of the above'
+    },
+    {
+        title: 'Methods are attached to the end of  ___ in JS?',
+        choices: ['loops', 'elements', 'objects', 'attributes'],
+        answer: 'objects'
+    }
+];
