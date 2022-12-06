323a4f2b828acdbdb1a436655619d6e061e582cf
cc-aeya <aaminah.x@icloud.com>
2022-12-06 09:59:45 +0000

    className show for qs to appear

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,32 +1,40 @@
 //Coding Logic 
 
 // when clicked start button must trigger the timer 
-// questions must shoow one at a time.
+// questions must show one at a time.
 // next question is ONLY shown when user clicks one of the option buttons for current question.
 // choices button does 2 things: sound for t/f and next q appears.
+
+// function for start button that starts quiz when clicked.
+
+var currentQuestionIndex = 0;
+var divQuestions = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var choicesOut = document.querySelector('#choices');
+
+
+// make var for current q where the value will be the questions array index
+
+function showQuestions() {
+    divQuestions.className = 'show';
+    var currentQuestion = questions[currentQuestionIndex];
+    var h2 = questionTitle.innerText = currentQuestion.title; 
+    // console.log(currentQuestion.title); 
+    
+    var choices = currentQuestion.choicesOut;
+    // console.log(currentQuestion.);
+}
+
+// showQuestions();
+
+
+
+
+
+
 
 //subtract time from the timer if ans is incorrect 
 // quiz should end if all q's are answered or when time runs out
 
-//feedback will show user if they are t/f use setTimeOut
+//feedback will show if user gets qs t/f use setTimeOut
 // setInterval will repeat after time is passed in.
-
-
-//use function for start button that starts quiz when clicked.
-// make var for current q where the value will be questions array index
-
-var currentQuestionIndex = 0;
-var questionWrapper = document.querySelector('#questions');
-var questionTitle = document.querySelector('#question-title');
-
-function startQuiz() {
-    var currentQuestion = questions[currentQuestionIndex];
-    questionTitle.innerText = currentQuestion.title;
-    //line 24 adds the question to the h2 element but its currently hidden, needs to show on button click.
-
-    console.log(currentQuestion);
-}
-
-startQuiz();
-
-//to output this in the browser you need to target html ids question div and give them innerTexts for e.g. the current question title
